0ec0aa919a88fd094dff82b1be3b79e76fe7226d
tatenda8is28 <agent-optimus>
2025-08-26 21:58:41 +0200

    fix: Implement GA dynamically via component

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <GoogleAnalyticsTracker />
     </BrowserRouter>
   </React.StrictMode>,
 )
